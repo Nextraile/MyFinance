25a56be2d0ff7200cef3bf0ff55eff60a639c740
Nextraile <nextraile@gmail.com>
2025-11-16 18:51:04 +0700

    Created API routes for authentication (login, register, logout), now the register route returns a token

---
+++ Backend/routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    // Route::get('/user');
+});
+
+
