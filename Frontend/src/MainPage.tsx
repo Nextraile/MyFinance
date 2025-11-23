bcc334313ccb2bec6519fa41d1090b285b6645aa
Mischiko Moe <panuwucake@gmail.com>
2025-11-23 09:52:58 +0700

    added create tracker system

--- Frontend/src/MainPage.tsx
+++ Frontend/src/MainPage.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 export function MainPage(): JSX.Element {
     return (
-        <div className="font-[Inter]">
+        <div className="font-[Inter] bg-background-primary min-h-screen">
             <Outlet />
         </div>
     )
--- Frontend/src/pages/dashboard/Dashboard.tsx
+++ Frontend/src/pages/dashboard/Dashboard.tsx
@@ -3,21 +3,37 @@ import { userData } from "@/lib/userData";
 import { faArrowRightFromBracket, faLock, faMagnifyingGlass, faSun, faUserPen } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { PlusIcon, XIcon } from "lucide-react";
-import { useState, type JSX } from "react";
+import { useRef, useState, type JSX } from "react";
 import { motion, AnimatePresence, spring } from "motion/react";
 
 export function Dashboard(): JSX.Element {
     const [ isAccountOpen, setIsAccountOpen ] = useState<boolean>(false)
+    const [ isCreateBoxOpen, setIsCreateBoxOpen ] = useState<boolean>(false)
+
+    const createBoxTitle = useRef<HTMLInputElement | null>(null)
+    const createBoxDescription = useRef<HTMLInputElement | null>(null)
+
+    const decideCreateBox = () => {
+        console.log("clicked");
+        if(createBoxTitle.current?.value === "" || createBoxDescription.current?.value === "") {
+            setIsCreateBoxOpen(false)
+            console.log("not created!")
+            return
+        }
+
+        setIsCreateBoxOpen(false)
+        console.log("created!")
+    }
 
     return (
-        <section className="flex flex-col items-center gap-5">
+        <section onClick={() => decideCreateBox()} className="flex flex-col items-center gap-5">
             <div className="flex justify-center">
                 <div className="flex justify-center items-center gap-2 mt-5 w-[85%] fixed">
                     <div className="flex items-center flex-1">
                         <label htmlFor="search" className="absolute pl-4 z-1">
-                            <FontAwesomeIcon icon={faMagnifyingGlass} className="" />
+                            <FontAwesomeIcon icon={faMagnifyingGlass} className="text-neutral-700" />
                         </label>
-                        <Input id="search" type="text" className="rounded-full h-10 pl-11 bg-neutral-50/30 focus:bg-neutral-50/50 backdrop-blur-[2px] backdrop-grayscale-50" placeholder="Search MyTracker" />
+                        <Input id="search" type="text" className="rounded-full h-10 pl-11 bg-white/50 focus:bg-neutral-50/80 backdrop-blur-[2px] backdrop-grayscale-50" placeholder="Search MyTracker" />
                     </div>
                     <motion.div>
                         <AnimatePresence mode="popLayout">
@@ -56,7 +72,7 @@ export function Dashboard(): JSX.Element {
                         <AnimatePresence>
                             {isAccountOpen && <motion.div 
                                 key="accountDetails"
-                                className="fixed right-0 sm:right-[4%] top-0 mt-18 mr-6 flex flex-col gap-3.5 bg-neutral-50/30 border-[0.5px] shadow p-3.5 rounded-xl backdrop-blur-[2px] backdrop-grayscale-50"
+                                className="fixed right-0 sm:right-[4%] top-0 mt-18 mr-6 flex flex-col gap-3.5 bg-neutral-50/40 border-[0.5px] shadow p-3.5 rounded-xl backdrop-blur-[2px] backdrop-grayscale-50"
                                 initial = {{
                                     x: 10,
                                     opacity: 0
@@ -90,19 +106,19 @@ export function Dashboard(): JSX.Element {
                                     </div>
                                 </div>
                                 <div className="flex flex-col gap-2 w-full">
-                                    <div className="flex items-center gap-2.5 bg-neutral-100 rounded-full py-2 px-4 w-full">
+                                    <div className="flex items-center gap-2.5 bg-green-500/20 rounded-full py-2 px-4 w-full">
                                         <FontAwesomeIcon icon={faSun}/>
                                         <p className="font-medium text-[15px]">Switch theme</p>
                                     </div>
-                                    <div className="flex items-center gap-2.5 bg-neutral-100 rounded-full py-2 px-4 w-full">
+                                    <div className="flex items-center gap-2.5 bg-green-500/20 rounded-full py-2 px-4 w-full">
                                         <FontAwesomeIcon icon={faUserPen} />
                                         <p className="font-medium text-[15px]">Edit Profile</p>
                                     </div>
-                                    <div className="flex items-center gap-2.5 bg-neutral-100 rounded-full py-2 px-4 w-full">
+                                    <div className="flex items-center gap-2.5 bg-green-500/20 rounded-full py-2 px-4 w-full">
                                         <FontAwesomeIcon icon={faLock} />
                                         <p className="font-medium text-[15px]">Change Password</p>
                                     </div>
-                                    <div className="flex items-center gap-2.5 bg-neutral-100 rounded-full py-2 px-4 w-full">
+                                    <div className="flex items-center gap-2.5 bg-green-500/20 rounded-full py-2 px-4 w-full">
                                         <FontAwesomeIcon icon={faArrowRightFromBracket} />
                                         <p className="font-medium text-[15px]">Signout</p>
                                     </div>
@@ -113,8 +129,17 @@ export function Dashboard(): JSX.Element {
                 </div>
             </div>
             <div className="flex flex-col sm:grid sm:grid-cols-2 lg:grid-cols-3 w-[87%] justify-center items-center gap-2.5 mt-15">
+                {isCreateBoxOpen && <div onClick={(e) => e.stopPropagation()} className="bg-white w-full flex-1 px-4 py-3 rounded-xl border-[1.5px]">
+                    <div className="flex flex-col gap-3.5">
+                        <div className="flex flex-col gap-0.5">
+                            <Input ref={createBoxTitle}  className="border-0 shadow-none font-semibold text-base p-0 m-0 focus-visible:ring-0" placeholder="Put your tittle here..." />
+                            <Input ref={createBoxDescription}  className="text-base font-normal p-0 m-0 border-0 shadow-none focus-visible:ring-0" placeholder="Put your description here..." />
+                        </div>
+
+                    </div>
+                </div>}
                 {userData.tracker.map(item => (
-                    <div className="bg-linear-to-t from-green-400/0 to-green-300/20 w-full flex-1 px-4 py-3 rounded-xl border-[1.75px]">
+                    <div className="bg-white w-full flex-1 px-4 py-3 rounded-xl border-[1.5px]">
                         <div className="flex flex-col gap-3.5">
                             <div className="flex flex-col gap-0.5">
                                 <h2 className="font-semibold text-base">{item.tittle}</h2>
@@ -131,8 +156,8 @@ export function Dashboard(): JSX.Element {
                     </div>
                 ))}
             </div>
-            <div className="flex justify-center items-center w-12 h-12 fixed bottom-0 right-0 mr-6 mb-8 rounded-md bg-neutral-50/30 backdrop-blur-[2px] backdrop-grayscale-50 sm:right-[4%] border-[0.5px] shadow">
-                <PlusIcon />
+            <div className="flex justify-center items-center w-12 h-12 fixed bottom-0 right-0 mr-6 mb-8 rounded-md bg-green-400/60 backdrop-blur-[2px] backdrop-grayscale-50 sm:right-[4%] border-[0.5px] shadow">
+                <PlusIcon onClick={(e) => {e.stopPropagation(); setIsCreateBoxOpen(true)}} className="text-white" size={30} />
             </div>
         </section>
     )
