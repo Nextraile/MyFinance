21405059f428d1490eaf90bb96d7e0809a5e31f6
Nextraile <nextraile@gmail.com>
2025-12-08 20:27:01 +0700

    Add image upload functionality to transaction creation

--- Backend/app/Http/Controllers/API/TransactionController.php
+++ Backend/app/Http/Controllers/API/TransactionController.php
@@ -13,6 +13,7 @@ use App\Models\Transaction;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 use Symfony\Component\HttpFoundation\Response;
 
 class TransactionController extends Controller
@@ -88,9 +89,21 @@ class TransactionController extends Controller
         try {
             $transaction = $request->all();
 
+            if ($request->hasFile('image')) {
+                $file = $request->file('image');
+                $name = time() . '' . preg_replace('/\s+/', '', $file->getClientOriginalName());
+                $path = $file->storeAs('transactions/'.'user-id_'.$request->user()->id.'/tracker-id_'.$request->route('tracker')->id, $name, 'public');
+                $image = 'transactions/'.'user-id_'.$request->user()->id. '/tracker-id_' . $request->route('tracker')->id . '/' . $name;
+                $transaction['image'] = $path;
+            }
+
             DB::transaction(function () use ($tracker, &$transaction) {
                 return $tracker->transactions()->create($transaction);
             });
+
+            if (isset($image)) {
+                $transaction['image'] = url(Storage::url($image));
+            }
 
             return ResponseHelper::createdResponse(
                 ['transaction' => $transaction],
