440d3871012a03f9a4a6d6c24c45e532e4876ce3
Mischiko Moe <panuwucake@gmail.com>
2025-12-09 22:49:25 +0700

    vercel omg

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -3,6 +3,9 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import routes from './MainRoute'
 import { ThemeProvider } from './components/theme-provider'
+import { ApiUrl } from './lib/variable'
+
+console.log("API URL being used:", ApiUrl);
 
 createRoot(document.getElementById('root')!).render(
   <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
