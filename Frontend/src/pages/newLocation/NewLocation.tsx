169829671233c70754fa527106d2089de79d7621
mischikomoe <panuwucake@gmail.com>
2026-05-01 17:58:57 +0700

    add: new location cofirmation UI

--- Frontend/src/pages/newLocation/NewLocation.tsx
+++ Frontend/src/pages/newLocation/NewLocation.tsx
@@ -1,7 +1,8 @@
-import { Button } from "@/components/ui/button";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Spinner } from "@/components/ui/spinner";
 import { faPencil } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { AlertCircleIcon } from "lucide-react";
 import { AnimatePresence, motion } from "motion/react";
 import { useEffect, useState, type JSX } from "react";
 import { useLocation, useParams } from "react-router-dom";
@@ -22,6 +23,17 @@ export function NewLocation(): JSX.Element {
         console.log("token :", token)
         console.log("search :", search)
     }, [])
+
+    const allowLoginHandler = () => {
+        setIsLoading(true)
+        setStatus("none")
+        setErrorMsg("")
+
+        setTimeout(() => {
+            setIsLoading(false)
+            setStatus("ok")
+        }, 4500)
+    }
 
     return (
         <section>
@@ -48,9 +60,116 @@ export function NewLocation(): JSX.Element {
                     <div className="flex flex-col items-center gap-3 -mt-5 px-5">
                         <FontAwesomeIcon icon={faPencil} className="text-6xl text-stone-900"></FontAwesomeIcon>
                         <h1 className="text-2xl font-bold text-stone-900 dark:text-background-primary">Device Login Request</h1>
-                        <p className="text-base text-stone-600 dark:text-stone-400 text-center">We receive a new device login request to {email}. <br/> If this isn't you, change your password immediately.</p>
-                        <Button className="mt-3 bg-transparent border-red-500 border-3 text-red-500 font-semibold w-40 sm:w-60">Allow Login</Button>
-                        <div>Saving changes <Spinner/></div>
+                        <p className="text-base text-stone-600 dark:text-stone-400 text-center mb-3">We receive a new device login request to {email}. <br/> If this isn't you, change your password immediately.</p>
+
+                        <AnimatePresence mode="wait">            
+                            { status == "error" &&
+                                <motion.div
+                                    key="error"
+                                    initial={{
+                                        x: 30,
+                                        opacity: 0,
+                                    }}
+                                    animate={{
+                                        x: 0,
+                                        opacity: 100,
+                                        transition: {
+                                            delay: 0.6
+                                        }
+                                    }}
+                                    exit={{
+                                        x: -30,
+                                        opacity: 0,
+                                        transition: {
+                                            delay: 0.1
+                                        }
+                                    }}
+                                >
+                                    <Alert variant="destructive" className="w-fit">
+                                        <AlertCircleIcon />
+                                        <AlertTitle className="font-semibold tracking-normal">Allow New Device Login Failed</AlertTitle>
+                                        <AlertDescription>
+                                            <ul className="list-inside list-disc text-sm">
+                                                <li>{errorMsg}</li>
+                                            </ul>
+                                        </AlertDescription>
+                                    </Alert>
+                                </motion.div>
+                            }   
+                            { status == "ok" &&
+                                <motion.div
+                                    key="ok"
+                                    className="flex flex-col items-center"
+                                    initial={{
+                                        x: 30,
+                                        opacity: 0,
+                                    }}
+                                    animate={{
+                                        x: 0,
+                                        opacity: 100,
+                                        transition: {
+                                            delay: 0.6
+                                        }
+                                    }}
+                                    exit={{
+                                        x: -30,
+                                        opacity: 0,
+                                    }}
+                                >
+                                    <p className="text-green-600 font-semibold text-center">Request allowed, you can now login with your new device. </p>
+                                    <p className="underline text-blue-500 text-center" onClick={() => {setIsOut(true); setTimeout(() => {window.location.href = "/access"}, 400)}}>Go back to login page</p>
+                                </motion.div>
+                            }               
+                            { !isLoading && status != "ok" &&
+                                <motion.button 
+                                    onClick={allowLoginHandler}
+                                    key="button"
+                                    className="bg-transparent border-red-500 border-3 text-red-500 font-semibold w-40 sm:w-60 rounded-md h-9 flex justify-center items-center"
+                                    type="button"
+                                    initial={{
+                                        x: 30,
+                                        opacity: 0,
+                                    }}
+                                    animate={{
+                                        x: 0,
+                                        opacity: 100,
+                                        transition: {
+                                            delay: 0.3
+                                        }
+                                    }}
+                                    exit={{
+                                        x: -30,
+                                        opacity: 0
+                                    }}
+                                >Allow Login</motion.button>
+                            }
+                            { isLoading &&                                
+                                <motion.div 
+                                    key="loading"
+                                    className="flex gap-2 items-center justify-center h-9 w-40 sm:w-60 font-semibold"
+                                    initial={{
+                                        x: 30,
+                                        opacity: 0,
+                                    }}
+                                    animate={{
+                                        x: 0,
+                                        opacity: 100,
+                                        transition: {
+                                            delay: 0.3
+                                        }
+                                    }}
+                                    exit={{
+                                        x: -30,
+                                        opacity: 0
+                                    }}
+                                >
+                                    Allowing request
+                                    <Spinner color="black"/>
+                                </motion.div>
+                            }
+                        </AnimatePresence>
+
+
                     </div>
                 </motion.div>}
             </AnimatePresence>
