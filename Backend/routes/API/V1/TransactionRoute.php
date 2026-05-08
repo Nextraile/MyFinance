893c48d449c74ced7c75798720e8f9e0ddc422c2
Nextraile <nextraile@gmail.com>
2026-05-08 14:50:34 +0700

    feat: update transaction routes to include authentication middleware and enhance resource definitions

--- Backend/routes/API/V1/TransactionRoute.php
+++ Backend/routes/API/V1/TransactionRoute.php
@@ -3,10 +3,23 @@
 use App\Http\Controllers\API\V1\TransactionController;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(TransactionController::class)->group(function () {
-    Route::prefix('trackers/{tracker}/transactions')->group(function () {
-        Route::post('', 'store');
-        Route::get('', 'index');
+Route::controller(TransactionController::class)->middleware('auth:sanctum')->group(function () {
+
+    Route::apiResource('transactions', TransactionController::class)->only(['show', 'update']);
+    Route::prefix('trackers/{tracker}/transactions')->name('transactions')->group(function () {
+        Route::post('', 'store')->name('.store');
+        Route::get('', 'index')->name('.index');
     });
-    Route::apiResource('transactions', TransactionController::class)->except(['index', 'store']);
+    Route::delete('transactions/{transaction}', 'delete')->name('transactions.delete');
+
+    Route::apiResource('deleted/transactions', TransactionController::class)->withTrashed()->only(['index', 'show'])->names([
+        'index' => 'deleted.transactions.index',
+        'show' => 'deleted.transactions.show',
+    ]);
+    
+    Route::prefix('deleted/transactions/{transaction}')->name('deleted.transactions')->group(function () {
+        Route::patch('/restore', 'restore')->withTrashed()->name('.restore');
+        Route::delete('/force', 'forceDelete')->withTrashed()->name('.force');
+    });
+    
 });
